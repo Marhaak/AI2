c02fd06934cb4d3d953b633ff4bcbbe68e754dc3
Marius <rodland92@hotmail.com>
2014-03-02 14:38:15 +0100

    more a stjerne

--- AI1/AI1/PathFinding.cpp
+++ AI1/AI1/PathFinding.cpp
@@ -1,40 +1,44 @@
-//#include "PathFinding.h"
-//
-//
-//PathFinding::PathFinding(void) {
-//
-//	m_initStartGoal = false;
-//	m_foundGoal = false;
-//}
-//
-//
-//PathFinding::~PathFinding(void) {
-//
-//}
-//
-//void PathFinding::FindPath(/*currentPos, targetPos*/) {
-//
-//	if(!m_initStartGoal) {
-//
-//		for(int i = 0; i < m_openList.size(); i++) {
-//
-//			delete m_openList[i];
-//		}
-//		m_openList.clear;
-//
-//		for(int i = 0; i < m_VisitedList.size; i++) {
-//
-//			delete m_VisitedList[i];
-//		}
-//		m_VisitedList.clear();
-//
-//		/*for(int i = 0; m_pathToGoal.size(); i++) {
-//
-//			m_pathToGoal[i];
-//		}
-//		m_pathToGoal.clear();*/
-//		seachCell start;
-//		start.x_coord = currentPos.x();
-//
-//	}
-//}
+#include "PathFinding.h"
+
+
+PathFinding::PathFinding(Enviorment* _enviornment) {
+
+	enviornment = _enviornment;
+	m_initStartGoal = false;
+	m_foundGoal = false;
+}
+
+
+PathFinding::~PathFinding(void) {
+
+}
+
+
+void PathFinding::FindPath(std::vector<Node*>* _followPath, Node* _startPos, Node* _targetPos) {
+
+	if(!m_initStartGoal) {
+
+		for(int i = 0; i < m_openList.size(); i++) {
+
+			delete m_openList[i];
+		}
+		m_openList.clear;
+
+		for(int i = 0; i < m_VisitedList.size; i++) {
+
+			delete m_VisitedList[i];
+		}
+		m_VisitedList.clear();
+
+		m_PathToGoal = _followPath;
+		for(int i = 0; m_PathToGoal->size(); i++) {
+
+			delete m_PathToGoal;
+		}
+		m_PathToGoal->clear();
+
+		Node start();
+		start.x_coord = currentPos.x();
+
+	}
+}
--- AI1/AI1/PathFinding.h
+++ AI1/AI1/PathFinding.h
@@ -1,31 +1,33 @@
-//#pragma once
-//#include <vector>
-//#include "Node.h"
-//
-//class PathFinding {
-//
-//public:
-//	PathFinding(void);
-//
-//	~PathFinding(void);
-//
-//	vector<Node>FindPath(Node* startPos, Node* targetPos);
-//	void ClearOpenList();
-//	void ClearVisitedList();
-//	void ClearPathToGoal();
-//	bool m_initStartGoal;
-//	bool m_foundGoal;
-//
-//private:
-//	void SetStartAndGoal(seachCell start, seachCell goal);
-//	void PathOpened(int x, int y, float newCost, seachCell* parent);
-//	seachCell *GetNextCell();
-//	void ContinuePath();
-//
-//	seachCell *m_startCell;
-//	seachCell *m_GoalCell;
-//	std::vector<seachCell*> m_openList;
-//	std::vector<seachCell*> m_VisitedList;
-//	//std::vector< std::vector<int> > m_PathToGoal;
-//};
-//
+#pragma once
+#include <vector>
+#include "Node.h"
+#include "Enviorment.h"
+
+class PathFinding {
+
+public:
+	PathFinding(Enviorment* _enviornment);
+
+	~PathFinding(void);
+
+	void FindPath(std::vector<Node> followPath, Node* startPos, Node* targetPos);
+	void ClearOpenList();
+	void ClearVisitedList();
+	void ClearPathToGoal();
+	bool m_initStartGoal;
+	bool m_foundGoal;
+
+private:
+	void SetStartAndGoal(Node start, Node goal);
+	void PathOpened(int x, int y, float newCost, Node* parent);
+	Node *GetNextCell();
+	void ContinuePath();
+
+	Node *m_startCell;
+	Node *m_GoalCell;
+	Enviorment* enviornment;
+	std::vector<Node*> m_openList;
+	std::vector<Node*> m_VisitedList;
+	//std::vector< std::vector<int> > m_PathToGoal;
+};
+
